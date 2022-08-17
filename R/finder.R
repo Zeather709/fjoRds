0498e58873b7635491a4b11de850cea6e46af07e
Zeather709 <heatherzurel@gmail.com>
2022-08-17 14:44:30 -0230

    Initial commit

---
+++ R/finder.R
@@ -0,0 +1,27 @@
+#' @param name 
+#'
+#' @description Check whether a name belongs to a fjord in Norway or Canada
+#' 
+#' @param name A string is a fjord name
+#' 
+#' @return a string sentence 
+#' 
+#' @export 
+#'
+#' @author Heather Zurel
+#' @title Fjord Finder
+#' 
+#' @example  fjord_finder("Sognefjord")
+#' 
+fjord_finder <- function(name) {
+  
+  stopifnot(is.character(name))
+  
+  if (name %in% fjords$name) {
+    print(glue::glue("{name} is a nice Norwegian fjord!"))
+  } else if (name %in% c("Princess Louisa Inlet", "Sechelt Inlet", "Skookumchuck Narrows")) {
+    print(glue::glue("{name} - I see you're in Canada, eh!"))
+  } else {
+    print(paste0(name, "? Never heard of em."))
+  }
+} 
---
+++ README.md
@@ -0,0 +1,28 @@
+
+# fjoRds
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of fjoRds is to determine whether a name is a fjord in Norway or Canada
+
+## Installation
+
+You can install the development version of fjoRds like so:
+
+``` r
+# remotes::install_github("Zeather709/fjoRds")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(fjoRds)
+## basic example code
+```
+
+## Code of Conduct
+
+Please note that the fjoRds project is released with a [Contributor Code of Conduct](https://contributor-covenant.org/version/2/1/CODE_OF_CONDUCT.html). By contributing to this project, you agree to abide by its terms.
